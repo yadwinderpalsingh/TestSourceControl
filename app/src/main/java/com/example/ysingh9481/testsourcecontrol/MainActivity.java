d58c587e546a4c6083d2b292b5f1f6cfc87a5d2d
Yadwinder Pal Singh <Ysingh9481@conestogac.on.ca>
2016-09-14 12:13:53 -0400

    Initial commit

---
+++ app/src/main/java/com/example/ysingh9481/testsourcecontrol/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ysingh9481.testsourcecontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
